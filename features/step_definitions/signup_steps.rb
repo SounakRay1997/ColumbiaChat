5ed4c7ac8af5dfb1e19c5e1de961e9f3ec74e49d
Sounak Ray <sounakray1997@gmail.com>
2021-11-03 19:59:29 -0400

    Update signup_steps.rb

--- features/step_definitions/signup_steps.rb
+++ features/step_definitions/signup_steps.rb
@@ -6,14 +6,10 @@ Then(/^I should reach the sign up page$/) do
   expect(page.body).to have_content("Sign up") and expect(page.body).to have_content("Confirmation")
 end
 
-Then(/^I fill up my details on the signup page$/) do
-  fill_in("user_username", :with => "Bruce_Wayne")
-  fill_in("user_email", :with => "batman@columbia.edu")
-  fill_in("user_password", :with => "wayne_enterprise")
-  fill_in("user_password_confirmation", :with => "wayne_enterprise")
-  click_button("Create my account")
-end
-
-Then(/^my details should be saved in the users database and there would be "([^"]*)" users in the database$/) do |arg|
-  User.count.should be arg.to_i
+Then(/^I fill in username as "([^"]*)", email as "([^"]*)" and password as "([^"]*)" on the signup page and database is updated$/) do |arg1, arg2, arg3|
+  fill_in("user_username", :with => arg1)
+  fill_in("user_email", :with => arg2)
+  fill_in("user_password", :with => arg3)
+  fill_in("user_password_confirmation", :with => arg3)
+  expect{click_button("Create my account")}.to change(User, :count).by(1)
 end
