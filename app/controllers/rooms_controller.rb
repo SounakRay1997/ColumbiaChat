df7af78c5878231ebd1aa608821fb64a77fab4ae
Umang Raj <umangraj@Umangs-Air.lan>
2021-11-16 03:52:23 -0500

    Private room

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -4,11 +4,17 @@ class RoomsController < ApplicationController
     redirect_to '/signin' unless @current_user
     @rooms = Room.public_rooms
     @users = User.all_except(@current_user)
+    @user_names = User.all_except(@current_user).pluck(:username)
+
     @room = Room.new
 
     @courses = Course.all 
     @departments = Course.distinct.pluck(:department_code)
-    @select_departments = Course.where(department_code: params["dept_id"]).pluck(:course_title, :course_subtitle).uniq!
+    if params["dept_id"].nil?
+      @select_departments = Course.all.pluck(:course_title, :course_subtitle).uniq!
+    else
+      @select_departments = Course.where(department_code: params["dept_id"]).pluck(:course_title, :course_subtitle).uniq!
+    end
     @select_departments.each do |n|
       if n[1].nil?
         n[1] = ""
@@ -19,7 +25,16 @@ class RoomsController < ApplicationController
   end
 
   def create
-    @room = Room.create(name: params["room"]["name"], is_private: false)
+    if params["room"]["private_room"] == "No"
+      @room = Room.create(name: params["room"]["name"], is_private: false)
+    else
+      @room = Room.create(name: params["room"]["name"], is_private: true)
+      puts params
+      params["room"]["selected_users"].each do |user|
+        user = User.where(username: user)
+        Participant.create(user_id: user.id, room_id: @room.ids)
+      end
+    end
   end
 
   def show
--- app/models/room.rb
+++ app/models/room.rb
@@ -3,10 +3,25 @@ class Room < ApplicationRecord
     scope :public_rooms, -> {where(is_private: false)}
     has_many :messages
     has_many :participants, dependent: :destroy
-    after_create_commit { broadcast_if_public }
+    after_create_commit { broadcast_if_public
+                          broadcast_to_users_if_private
+     }
 
  def broadcast_if_public
   broadcast_append_to "rooms" unless self.is_private
+ end
+
+ def broadcast_to_users_if_private
+  puts "potato2"
+  puts Participant.where(room_id: Room.where(name: self.name).ids[0]).ids
+  # private_users = Participant.joins(:user).where(users: {room_id: Participant.joins(:room).where(rooms: {name: self.name})})
+  private_users = Participant.where(room_id: Room.where(name: self.name).ids[0])
+  # private_users = Participant.where(room_id: Room.where(name: self.name).pluck(:id)).pluck(:user_id)
+  if self.is_private
+    private_users.each do |user|
+      broadcast_append_to "rooms"
+    end
+  end
  end
 
  def self.create_private_room(users, room_name)
