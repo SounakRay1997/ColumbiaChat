fbe6c6618fc15f0e03eaf2abbce404e916c9ded4
Sounak Ray <sounakray1997@gmail.com>
2021-11-17 13:44:08 -0500

    email confirmation working

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,6 +1,7 @@
 class UserMailer < ActionMailer::Base
   default :from => "columbiachat@gmail.com"
   layout 'mailer'
+
   def registration_confirmation(user)
     @user = user
     mail(:to => "#{user.name} <#{user.email}>", :subject => "Registration Confirmation")
