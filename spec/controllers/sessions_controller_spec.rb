920457c61d0e414262122bfa85dfb3ebe91c5351
Sounak Ray <sounakray1997@gmail.com>
2021-11-16 21:45:11 -0500

    modified some tests

--- spec/controllers/sessions_controller_spec.rb
+++ spec/controllers/sessions_controller_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
 describe SessionsController, type: :controller do
-    let(:user_params) { { username: 'Umang_Raj', password: 'test', email_confirmed: 1 } }
+    let(:user_params) { { username: 'Umang_Raj', password: 'test' } }
     let(:params) { {session: user_params} }
     subject { post signin_path, params: params}
     render_views
@@ -14,7 +14,13 @@ describe SessionsController, type: :controller do
         post(:create, params: params)
         expect(response).to redirect_to(root_path)
         expect(response).to have_http_status(302)
-      end
+    end
+
+    it 'logs in existing user whose email is not confirmed' do
+        post(:create, params: params)
+        expect(response).to redirect_to(root_path)
+        expect(response).to have_http_status(302)
+    end
 
     it 'logs in non-existing user' do 
         post(:create, params: {session: {username: "banana", password: "lol", email_confirmed: 1}})
@@ -43,4 +49,39 @@ describe SessionsController, type: :controller do
         expect(response).to render_template('signup')
     end 
 
-end 
+end
+
+describe SessionsController, type: :controller do
+    let(:user_params) { { username: 'Umang_Raj', password: 'test' } }
+    let(:params) { {session: user_params} }
+    subject { post signin_path, params: params}
+    render_views
+
+    before(:each) do
+        @user = User.create({username: "Umang_Raj", email: "umang@columbia.edu", password: "test", email_confirmed: 0})
+    end
+
+    it 'does not log in an existing user' do
+        post(:create, params: params)
+        expect(response).to redirect_to('/signin')
+    end
+
+end
+
+describe SessionsController, type: :controller do
+    let(:user_params) { { username: 'Umang_Raj', password: 'test123' } }
+    let(:params) { {session: user_params} }
+    subject { post signin_path, params: params}
+    render_views
+
+    before(:each) do
+        @user = User.create({username: "Umang_Raj", email: "umang@columbia.edu", password: "test", email_confirmed: 0})
+    end
+
+    it 'does not log in an existing user with wrong password' do
+        post(:create, params: params)
+        expect(response).to redirect_to('/signin')
+    end
+
+end
+
