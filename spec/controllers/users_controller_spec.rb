b3a0a7a2bd94fcb0836b3b59b1019d0ad0c896a6
Harry <hsg35@miami.edu>
2021-11-17 17:39:38 -0500

    host url change

---
+++ spec/controllers/users_controller_spec.rb
@@ -0,0 +1,11 @@
+require 'rails_helper'
+
+describe UsersController, type: :controller do
+
+    it 'creates a new room' do
+      x = [1,2,3]
+      expect(x).to include(1)
+      
+    end
+
+end 
