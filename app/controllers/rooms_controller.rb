31bf33c620f7a9566d9ee7cbf1131c1290e9c9ff
Harry <hsg35@miami.edu>
2021-11-17 22:50:23 -0500

    rooms

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -12,13 +12,13 @@ class RoomsController < ApplicationController
 
   def create
     @current_user = current_user; redirect_to '/signin' unless @current_user
-    if current_user.lat && current_user.long 
+    if current_user && current_user.lat && current_user.long 
       room_lat = current_user.lat;room_long = current_user.long
     else 
       room_lat = 0;room_long = 0
     end
     @room = Room.create(distance: params["room"]["distance"], name: params["room"]["name"], is_private: false, lat: room_lat, long: room_long)
-    redirect_to :back
+    
   end
 
   @miles_conv = 
@@ -26,13 +26,10 @@ class RoomsController < ApplicationController
   
 
   def isInRadius(lat1, lon1, user_lat, user_long, roomDist) 
-    dis_miles = Geocoder::Calculations.distance_between([lat1,lon1], [user_lat,user_long])
-    dis_feet = dis_miles * 5280.0
-    return roomDist >= dis_feet 
+    dis_miles = Geocoder::Calculations.distance_between([lat1,lon1], [user_lat,user_long]);dis_feet = dis_miles * 5280.0;return roomDist >= dis_feet 
   end
 
   def show
-    @current_user = current_user;@single_room = Room.find(params[:id]);public_rooms = Room.public_rooms;user_lat = current_user.lat;user_long = current_user.long;tmp_rooms = []
-    public_rooms.each  { |room| room_dist_req = room.distance;lat1 = room.lat;lon1 = room.long;tmp_rooms.append(room) if isInRadius(user_lat, user_long, lat1, lon1, room_dist_req) } ;@rooms = tmp_rooms;@users = User.all_except(@current_user);@room = Room.new;@message = Message.new;@messages = @single_room.messages;render "index"
+    @current_user = current_user;@single_room = Room.find(params[:id]);public_rooms = Room.public_rooms;user_lat = current_user.lat;user_long = current_user.long;tmp_rooms = [];public_rooms.each  { |room| room_dist_req = room.distance;lat1 = room.lat;lon1 = room.long;tmp_rooms.append(room) if isInRadius(user_lat, user_long, lat1, lon1, room_dist_req) } ;@rooms = tmp_rooms;@users = User.all_except(@current_user);@room = Room.new;@message = Message.new;@messages = @single_room.messages;render "index"
   end
 end
